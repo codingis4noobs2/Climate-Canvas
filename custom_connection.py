4fa933ea7b79d4f88fd673f73dca70f6aedfc6d3
Parth Shah <87560178+codingis4noobs2@users.noreply.github.com>
2023-07-31 16:40:05 +0530

    Create custom_connection.py

---
+++ custom_connection.py
@@ -0,0 +1,30 @@
+from streamlit.connections import ExperimentalBaseConnection
+import requests
+
+
+class OpenWeatherApiConnection(ExperimentalBaseConnection):
+
+    def _connect(self, **kwargs) -> requests.Session:
+        session = requests.Session()
+        session.params['appid'] = kwargs['api_key']
+        return session
+
+    def get_weather_by_city_name(self, city_name):
+        # API endpoint for getting weather by city name
+        weather_url = "http://api.openweathermap.org/data/2.5/weather"
+
+        # Set up the parameters
+        params = {
+            "q": city_name,
+        }
+
+        # Make the API call using the 'requests' library
+        response = self._instance.get(api_url, params=params)
+
+        # Check if the API call was successful (status code 200)
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 404:
+            return None
+        else:
+            return None
